8d7cb5cf3dce95446e5d5ab087d246f1e2703efb
aadneny <aadneny@stud.ntnu.iie.no>
2018-11-17 17:51:09 +0100

    Added comments and more Moved travis Added pagination Made a reusable caseFeed component Added Rating Fixed comments refreshing on submit' Added test: DAO - getCategory.length

--- server/src/dao/testing/create_testdata.sql
+++ server/src/dao/testing/create_testdata.sql
@@ -5,7 +5,7 @@ INSERT INTO kategori (kategori) VALUES
 INSERT INTO sak (aktiv, bilde, bildetekst, innhold, kategori, overskrift, tidspunkt, viktighet) VALUES
 (1, 'www.bilde1.no.jpg', 'bidletekst1', 'innhold1', 'sport', 'overksrift1', 'tidspunkt1', 1),
 (1, 'www.bilde2.no.jpg', 'bidletekst2', 'innhold2', 'økonomi', 'overksrift2', 'tidspunkt2', 1),
-(0, 'www.bilde3.no.jpg', 'bidletekst3', 'innhold3', 'annet', 'overksrift3', 'tidspunkt3', 2);
+(0, 'www.bilde3.no.jpg', 'bidletekst3', 'innhold3', 'annet', 'overksrift3', 'tidspunkt3', 2),
 (1, 'www.bilde4.no.jpg', 'bidletekst4', 'innhold4', 'økonomi', 'overksrift4', 'tidspunkt2', 1),
 (1, 'www.bilde5.no.jpg', 'bidletekst5', 'innhold5', 'annet', 'overksrift5', 'tidspunkt3', 1);
 (1, 'www.bilde6.no.jpg', 'bidletekst6', 'innhold6', 'økonomi', 'overksrift6', 'tidspunkt2', 1),
