77e187a27bdab33c83725f4b4a14f9b860ec3016
aadneny <aadneny@stud.ntnu.iie.no>
2018-11-20 15:01:26 +0100

    Added comments and more Moved travis Added pagination Made a reusable caseFeed component Added Rating Fixed comments refreshing on submit' Added test: DAO - getCategory.length

--- client/tests/widgetsTest.js
+++ client/tests/widgetsTest.js
@@ -37,15 +37,16 @@ describe('Testing card', () => {
   });
 
   it('Testing if the cardpreview contains overskrift', () => {
-    expect((wrapper.find("card-title").text())).toEqual('Karpe til Festningen-festival');
+    expect(wrapper.find("card-title").text()).toEqual('Karpe til Festningen-festival');
+
   });
 
   it('Testing if the cardpreview contains an image', () => {
-    expect((wrapper.find("card-img-top").text())).toEqual('bildeadresse.jpg');
+    expect(wrapper.find("card-img-top").text()).toEqual('bildeadresse.jpg');
   });
 
   it('Testing if the cardpreview', () => {
-    expect((wrapper.find("card-img-top").text())).toEqual('bildeadresse.jpg');
+    expect(wrapper.find("card-img-top").text()).toEqual('bildeadresse.jpg');
   });
 
 });
